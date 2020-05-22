73e09f170bf80637d0a78e93eded94a6a8afde2f
OlgaEvdokimova <olgaaevdokim@gmail.com>
2020-05-23 00:36:20 +0300

    added 3) after correcting 1 and 2

--- src/task4/Main.java
+++ src/task4/Main.java
@@ -70,96 +70,37 @@ public class Main {
 
 
         Map<Integer, Integer> map2 = new HashMap<>();
-        for (int i = 0; i < splitText.length ; i++) {
+        for (int i = 0; i < splitText.length; i++) {
             if (!(map2.containsKey(splitText[i].length()))) {
                 map2.put(splitText[i].length(), 0);
             }
             map2.put(splitText[i].length(), map2.get(splitText[i].length()) + 1);
 
-    }
+        }
 
         System.out.println("отсортировать вывод от меньшей длины к большей");
-                for(Map.Entry<Integer, Integer> el:map2.entrySet()){
-        System.out.println(el.getKey()+" - "+el.getValue());
+        for (Map.Entry<Integer, Integer> el : map2.entrySet()) {
+            System.out.println(el.getKey() + " - " + el.getValue());
         }
 
+// 3)
 
+        Set<String> set = new HashSet<>();
+        System.out.println("добавить сами слова");
+        for (Map.Entry<Integer, Integer> el : map2.entrySet()) {
 
+            for (int i = 0; i < splitText.length; i++) {
+                if (splitText[i].length() == el.getKey()) {
+                    set.add(splitText[i]);
+                }
+            }
+            System.out.println(el.getKey() + " - " + el.getValue() + " (" + set +" )");
+            set.clear();
         }
+    }
+
+}
 
 
-        }
 
 
-//
-//    public static void main(String[] args) {
-//        String[] splitText = TEXT.split("\\s+");
-//        Set<String> set = new HashSet<>(Arrays.asList(splitText));
-//        List<String> list = new ArrayList<>(Arrays.asList(splitText));
-//        HashMap<String, Integer> map = new HashMap<>();
-//        Iterator<String> i = set.iterator();
-//        int count;
-//        while (i.hasNext()) {
-//            String elemOfSet = i.next();
-//            count = 0;
-//            for (int k = 0; k < list.size(); k++) {
-//                if (elemOfSet.compareTo(list.get(k)) == 0) {
-//                    count++;
-//                }
-//            }
-//            map.put(elemOfSet, count);
-//        }
-//        System.out.println("Map");
-//        System.out.println(map);
-//
-//// descending sort by values
-//        List<Map.Entry<String, Integer>> list1 = new ArrayList<>(map.entrySet());
-//        for (int j = 0; j < list1.size(); j++) {
-//            for (int n = 0; n < list1.size() - j - 1; n++) {
-//                if (list1.get(n).getValue() < list1.get(n + 1).getValue()) {
-//                    Collections.swap(list1, n, n + 1);
-//                }
-//            }
-//        }
-//        System.out.println("отсортировать по убыванию частоты встречание");
-//        for (Map.Entry<String, Integer> el : list1) {
-//            System.out.println(el.getKey() + " - " + el.getValue());
-//        }
-//
-//
-//// ascending sort
-//        for (int j = 0; j < list1.size(); j++) {
-//            for (int n = 0; n < list1.size() - j - 1; n++) {
-//                if (list1.get(n).getValue() > list1.get(n + 1).getValue()) {
-//                    Collections.swap(list1, n, n + 1);
-//                }
-//            }
-//        }
-//
-//        int length = 0;
-//        int countWord;
-//        List<Map.Entry<String, Integer>> list2 = new ArrayList<>();
-//        Map<Integer, Integer> map2 = new HashMap<>();
-//
-//        Iterator<Map.Entry<String, Integer>> iterator = list1.iterator();
-//        while (iterator.hasNext()) {
-//            Map.Entry<String, Integer> elemOfIterator = iterator.next();
-//            length = elemOfIterator.getKey().length();
-//            countWord = 0;
-//            for (int n = 1; n < list1.size(); n++) {
-//                if (length == list1.get(n).getKey().length()) {
-//                    countWord = elemOfIterator.getValue() + list1.get(n).getValue();
-//                }
-//            }
-//            map2.put(length, countWord);
-//            System.out.println(map2);
-//        }
-//        System.out.println(map2);
-//        System.out.println("отсортировать вывод от меньшей длины к большей");
-//        for (Map.Entry<Integer, Integer> el : map2.entrySet()) {
-//            System.out.println(el.getKey() + " - " + el.getValue());
-//        }
-//        //System.out.println(map2);
-//
-//
-//    }
