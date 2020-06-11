ce6cd37fde13472f7da3e4b070dfa62fc9dbf1dc
OlgaEvdokimova <olgaaevdokim@gmail.com>
2020-06-12 00:21:50 +0300

    Correct Input and Main

--- src/figure/Input.java
+++ src/figure/Input.java
@@ -19,36 +19,40 @@ public class Input {
     }
 
 
-
     public Figure getFigure() throws WrongDataException {
         Scanner scan = new Scanner(System.in);
         Figure figure = null;
         System.out.println("Введите фигуру");
-        String strFigure = scan.nextLine();
-        if (strFigure.equals("Circle")) {
-            System.out.println("Введите параметры x, y, radius");
-            String x = scan.nextLine();
-            String y = scan.nextLine();
-            String radius = scan.nextLine();
-            try {
+        String strFigure;
+        try {
+            strFigure = scan.nextLine();
+            if (strFigure.equals("Circle")) {
+                System.out.println("Введите параметры x, y, radius");
+                String x = scan.nextLine();
+                String y = scan.nextLine();
+                String radius = scan.nextLine();
+                double xC = Double.parseDouble(x);
+                double yC = Double.parseDouble(y);
                 double radiusC = Double.parseDouble(radius);
                 figure = new Circle(radiusC);
-            } catch (NumberFormatException ex) {
-                throw new WrongDataException("Incorrect data");
-            }
-        } else if (strFigure.equals("Rectangle")) {
-            System.out.println("Введите параметры x, y, width, height");
-            String x = scan.nextLine();
-            String y = scan.nextLine();
-            String width = scan.nextLine();
-            String height = scan.nextLine();
-            try {
+
+            } else if (strFigure.equals("Rectangle")) {
+                System.out.println("Введите параметры x, y, width, height");
+                String x = scan.nextLine();
+                String y = scan.nextLine();
+                String width = scan.nextLine();
+                String height = scan.nextLine();
+                double xR = Double.parseDouble(x);
+                double yR = Double.parseDouble(y);
                 double widthR = Double.parseDouble(width);
                 double heightR = Double.parseDouble(height);
                 figure = new Rectangle(widthR, heightR);
-            } catch (NumberFormatException ex) {
-                throw new WrongDataException("Incorrect data");
+
+            } else {
+                throw new WrongDataException("Incorrect figure");
             }
+        } catch (NumberFormatException ex) {
+            throw new WrongDataException("Incorrect data");
         }
 
         return figure;
--- src/figure/Main.java
+++ src/figure/Main.java
@@ -8,88 +8,61 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println(" 1 - програмно, 2 - через консоль, 3 - через файл");
-        int choice = scanner.nextInt();
+        String choice = scanner.nextLine();
 
         List<Figure> figures = new ArrayList<>();
+        try {
+            if (choice.equals("1")) {
+                Figure circle1 = new Circle(1);
+                Figure rectangle1 = new Rectangle(3, 2);
 
-        if (choice == 1) {
-            Figure circle1 = new Circle(1);
-            Figure rectangle1 = new Rectangle(3, 2);
-
-            Figure circle2 = new Circle(4);
-            Figure rectangle2 = new Rectangle(4, 2);
-            Figure circle3 = new Circle(7);
-            Figure rectangle3 = new Rectangle(1, 2);
+                Figure circle2 = new Circle(4);
+                Figure rectangle2 = new Rectangle(4, 2);
+                Figure circle3 = new Circle(7);
+                Figure rectangle3 = new Rectangle(1, 2);
 
 
-            figures.add(circle1);
-            figures.add(rectangle1);
-            figures.add(circle2);
-            figures.add(rectangle2);
-            figures.add(circle3);
-            figures.add(rectangle3);
-        } else if (choice == 2) {
-            Input input = new Input();
-            try {
+                figures.add(circle1);
+                figures.add(rectangle1);
+                figures.add(circle2);
+                figures.add(rectangle2);
+                figures.add(circle3);
+                figures.add(rectangle3);
+            } else if (choice.equals("2")) {
+                Input input = new Input();
                 input.callFigures(figures);
 
-            } catch (WrongDataException ex) {
-                ex.printStackTrace();
+            } else if (choice.equals("3")) {
+                FileUtils fileUtils = new FileUtils();
+                    fileUtils.getFigures(figures);
+            } else {
+
+                    throw new WrongDataException("1 or 2 or 3");
+
             }
-        } else if (choice == 3) {
-            FileUtils fileUtils = new FileUtils();
-            try {
-               fileUtils.getFigures(figures);
-            } catch (WrongDataException e) {
-                e.printStackTrace();
+            System.out.println("Сортировка по площади");
+            Comparator<Figure> compare1 = new Compare1();
+            Collections.sort(figures, compare1);
+            for (Figure figure : figures) {
+                System.out.println(figure);
             }
-        } else {
-            scanner.close();
+            System.out.println();
+            System.out.println("Сортировка по периметру");
+            Comparator<Figure> compare2 = new Compare2();
+            Collections.sort(figures, compare2);
+            for (Figure figure : figures) {
+                System.out.println(figure);
+            }
+
+            System.out.println();
+            System.out.println("Сортировка по типу и площади");
+            Comparator<Figure> compare3 = new Compare3();
+            Collections.sort(figures, compare3);
+            for (Figure figure : figures) {
+                System.out.println(figure);
+            }
+        } catch (WrongDataException e) {
+            e.printStackTrace();
         }
-
-        System.out.println("Сортировка по площади");
-        Comparator<Figure> compare1 = new Compare1();
-        Collections.sort(figures, compare1);
-        for (Figure figure : figures) {
-            System.out.println(figure);
         }
-        System.out.println();
-        System.out.println("Сортировка по периметру");
-        Comparator<Figure> compare2 = new Compare2();
-        Collections.sort(figures, compare2);
-        for (Figure figure : figures) {
-            System.out.println(figure);
-        }
-
-        System.out.println();
-        System.out.println("Сортировка по типу и площади");
-        Comparator<Figure> compare3 = new Compare3();
-        Collections.sort(figures, compare3);
-        for (Figure figure : figures) {
-            System.out.println(figure);
-        }
-
-
-//
-//        System.out.println("Сортировка по площади Input");
-//            Collections.sort(figuresInput, compare1);
-//            for (Figure figure : figuresInput) {
-//                System.out.println(figure);
-//            }
-//
-//            System.out.println("Сортировка по периметру Input");
-//            Collections.sort(figuresInput, compare2);
-//            for (Figure figure : figuresInput) {
-//                System.out.println(figure);
-//            }
-//
-//            System.out.println("Сортировка по типу и площади");
-//            Collections.sort(figuresInput, compare3);
-//            for (Figure figure : figuresInput) {
-//                System.out.println(figure);
-//            }
-
-
     }
-}
-
